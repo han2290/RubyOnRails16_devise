4bb88b0b1ac6e854b0b7c7415faabad82b2adf5d
Hanum <cogksdna@naver.com>
2018-07-02 05:09:08 +0000

    first commit

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+# 이 부분을 제일 많이 사용하게 됨
+# sns 로그인 할 때, 로그인 함으로써 가져온 데이터를 어떻게 정리할 것인가?
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  # You should configure your model like this:
+  # devise :omniauthable, omniauth_providers: [:twitter]
+
+  # You should also create an action method in this controller like this:
+  # def twitter
+  # end
+
+  # More info at:
+  # https://github.com/plataformatec/devise#omniauth
+
+  # GET|POST /resource/auth/twitter
+  # def passthru
+  #   super
+  # end
+
+  # GET|POST /users/auth/twitter/callback
+  # def failure
+  #   super
+  # end
+
+  # protected
+
+  # The path used when OmniAuth fails
+  # def after_omniauth_failure_path_for(scope)
+  #   super(scope)
+  # end
+end
